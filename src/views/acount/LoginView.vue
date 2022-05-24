944ff40694aa8d22ff3da6ae5ffb2121f6b1cb94
Wv98-web <418060258@qq.com>
2022-05-24 11:28:33 +0800

    init

---
+++ src/views/acount/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">登录</div>
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
